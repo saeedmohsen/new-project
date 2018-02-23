3c5c7117ca15e97d863772858a7ca7dbd04044c2
saeed-mohsen-bahbry <saeed_200911@hotmail.com>
2018-02-23 17:45:43 +0300

    new

---
+++ js.js
@@ -0,0 +1,113 @@
+
+/*
+var myAge = prompt("كم عمرك الان");
+
+if( myAge > 10 ){
+	document.write("لايسمح لك بالدخول " + "وشكرا على زيارتك ");
+
+} else if (myAge == 10) { //
+	document.write("يسمح لك بالدخول فقط انتبه للالفاظك " + "شكرا لك ");
+
+} else {
+	document.write("لاتقبل الدخول الى الموقع وشكرا ");
+}
+
+*/
+
+
+
+
+
+/*
+var name ="saeed",
+    age = "yemen",
+    contr = "yemen";
+
+if (name == "saeed" && age == "we" && contr == "yemen") {
+	alert(name + " "+ age + " " + contr);
+}    else {
+	alert("ناسف على عدم تتطابق البيانات وشكرا ");
+} 
+*/
+
+
+/*
+
+function sae() {
+
+	"use strict";
+
+	var myAge = prompt("كم عمرك الان");
+
+if( myAge > 10 ){
+	document.write("لايسمح لك بالدخول " + "وشكرا على زيارتك ");
+
+} else if (myAge == 10) { //
+	document.write("يسمح لك بالدخول فقط انتبه للالفاظك " + "شكرا لك ");
+
+} else {
+	document.write("لاتقبل الدخول الى الموقع وشكرا ");
+}
+
+}
+
+
+*/
+
+
+
+
+
+/*
+function saeed() {
+
+	"use strict";
+
+	
+	return "انا احب اليمن";
+}
+
+
+document.getElementById("saeed").innerHTML = saeed();
+*/
+
+
+
+
+
+
+/*
+function say( myName, age,) {
+
+	"use strict";
+
+	return "مرحبا وسهلا " + myName + " " + age + "كيف حالك اليوم ";
+
+}
+
+document.getElementById("saeed").innerHTML = say("saeed mohsen", 24,);
+
+
+*/
+
+  
+
+
+/*
+function say(dis) {
+
+	"use strict";
+
+	var sho = 200
+
+	return dis - sho;
+
+}
+
+var ase = prompt("خصم المنتج ")
+
+document.getElementById("saeed").innerHTML = say(ase);
+*/
+
+
+
